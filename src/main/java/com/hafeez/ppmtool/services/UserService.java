172b65b002ace675d704d29305a625c90bee5ac6
hafeezaslam <hafeez_khanu@yahoo.com>
2021-03-25 20:38:59 +0500

    updating and resolving errors

--- src/main/java/com/hafeez/ppmtool/services/UserService.java
+++ src/main/java/com/hafeez/ppmtool/services/UserService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLSyntaxErrorException;
-
 @Service
 public class UserService {
 
