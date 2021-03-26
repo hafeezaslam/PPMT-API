43033f948a0a611e52d2eeadd91dc49e344b7343
hafeezaslam <hafeez_khanu@yahoo.com>
2021-03-26 19:29:04 +0500

    adding frontend

---
+++ src/main/resources/static/precache-manifest.1596156d92c7fbdb4c16ab87fdb2a8db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5234b05742d3116bcc15eaece36733ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "028e74e4fd5faebfcb98",
+    "url": "/static/css/main.ea51fd30.chunk.css"
+  },
+  {
+    "revision": "11347bc7371944ab2b0f",
+    "url": "/static/js/2.a5acbe13.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.a5acbe13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028e74e4fd5faebfcb98",
+    "url": "/static/js/main.3bf2783e.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
